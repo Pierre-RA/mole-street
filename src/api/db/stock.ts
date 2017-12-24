5aff8b58d567cd6e3b5e24a23be926a2bbdf2b78
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-12-24 12:26:02 +0100

    feat: add get last entries

--- src/api/db/stock.ts
+++ src/api/db/stock.ts
@@ -2,12 +2,14 @@ import * as Mongoose from 'mongoose';
 
 import { Stock } from '../../shared';
 
-interface StockModel extends Stock, Mongoose.Document {}
+interface StockModel extends Stock, Mongoose.Document {
+  findLast(): Array<Stock>;
+}
 
 export let stockSchema = new Mongoose.Schema({
   name: String,
-  initials: String,
-  time: Number,
+  initials: { type: String, index: true },
+  time: { type: Number, index: true },
   volume: Number,
   high: Number,
   low: Number,
@@ -17,5 +19,26 @@ export let stockSchema = new Mongoose.Schema({
   prev: Number,
   change: Number
 });
+
+stockSchema.statics.findLast = function() {
+  return this.model('Stock').aggregate([{
+    $sort: { initials: 1, time: -1}
+  }, {
+    $group: {
+      _id: '$initials',
+      name: {$first: '$name'},
+      initials: {$first: '$initials'},
+      time: {$first: '$time'},
+      volume: {$first: '$volume'},
+      high: {$first: '$high'},
+      low: {$first: '$low'},
+      open: {$first: '$open'},
+      close: {$first: '$close'},
+      last: {$first: '$last'},
+      prev: {$first: '$prev'},
+      change: {$first: '$change'},
+    }
+  }]);
+};
 
 export let DBStock = Mongoose.model<StockModel>('Stock', stockSchema);
--- src/api/routes/stocks.ts
+++ src/api/routes/stocks.ts
@@ -20,11 +20,38 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 /**
- * GET /:id
- * get one stock
+ * GET /last
+ * get list of last values
  */
-router.get('/:id', (req: Request, res: Response) => {
-  DBStock.findOne({ _id: req.params.id })
+router.get('/last', (req: Request, res: Response) => {
+  DBStock.aggregate([{
+    $sort: { initials: 1, time: -1}
+  }, {
+    $group: {
+      _id: '$initials',
+      name: {$first: '$name'},
+      initials: {$first: '$initials'},
+      time: {$first: '$time'},
+      volume: {$first: '$volume'},
+      high: {$first: '$high'},
+      low: {$first: '$low'},
+      open: {$first: '$open'},
+      close: {$first: '$close'},
+      last: {$first: '$last'},
+      prev: {$first: '$prev'},
+      change: {$first: '$change'},
+    }
+  }])
+    .then(doc => {
+      res.json(doc);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get('/last/:initials', (req: Request, res: Response) => {
+  DBStock.find({ initials: req.params.initials }).sort({ time: -1 }).limit(1)
     .then(doc => {
       res.json(doc);
     })
@@ -34,11 +61,11 @@ router.get('/:id', (req: Request, res: Response) => {
 });
 
 /**
- * DELETE /:id
- * remove one stock
+ * GET /:id
+ * get one stock
  */
-router.delete('/:id', (req: Request, res: Response) => {
-  DBStock.findOneAndRemove({ _id: req.params.id })
+router.get('/:id', (req: Request, res: Response) => {
+  DBStock.findOne({ _id: req.params.id })
     .then(doc => {
       res.json(doc);
     })
