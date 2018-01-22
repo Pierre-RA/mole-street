a8b01fcf6fb67aa4c83c3710204c3bbfa965a7a1
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2018-01-22 08:26:05 +0100

    chore: add loading spinner

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
 
@@ -13,14 +14,17 @@ export class SignInComponent implements OnInit {
   form: FormGroup;
   message: string;
   loaded: boolean;
+  loading: boolean;
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.loaded = false;
+    this.loading = false;
     this.initForm();
   }
 
@@ -32,14 +36,17 @@ export class SignInComponent implements OnInit {
   }
 
   onSubmit() {
+    this.loading = true;
     this.authService.getAccessToken(this.form.value.email, this.form.value.password)
       .subscribe(token => {
         this.loaded = true;
+        this.loading = false;
         if (!token) {
           this.message = 'Wrong credentials.';
         } else {
           this.authService.setAccessToken(token);
           this.message = null;
+          this.router.navigate(['']);
         }
       });
   }
