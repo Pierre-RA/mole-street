3b8a733dd49cfa4a0c53f6158ee3eabeed0d3af1
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2018-01-29 18:30:00 +0100

    feat: add profile info

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard.component';
 import { UsersService } from '../users/users.service';
@@ -8,6 +9,8 @@ import { UsersService } from '../users/users.service';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: '', component: DashboardComponent, pathMatch: 'full' }
     ])
